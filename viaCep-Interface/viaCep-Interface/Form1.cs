49df5038614b6c088505980c14d978cdf4c06796
Felipe Romio <113053407+feliperomio@users.noreply.github.com>
2025-10-21 11:51:02 -0300

    viaCep com Interface

---
+++ viaCep-Interface/viaCep-Interface/Form1.cs
@@ -0,0 +1,30 @@
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace viaCep_Interface
+{
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private async void btnBuscar_Click(object sender, EventArgs e) {        //para botão que faz um get na API, lembrar de botar async antes do void senao n funciona
+
+
+            string url = $"https://viacep.com.br/ws/{textBoxCep.Text}/json/";
+
+            HttpClient client = new HttpClient();
+
+            HttpResponseMessage resposta = await client.GetAsync(url);
+
+            string json = await resposta.Content.ReadAsStringAsync();
+
+            Endereco endereco = JsonSerializer.Deserialize<Endereco>(json);
+
+            dataGridView1.DataSource = new List<Endereco> { endereco };     //exibe no datagridview
+
+
+        }
+    }
+}
